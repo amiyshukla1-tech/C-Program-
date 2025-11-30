8de284d55c99bf3cc280e2c56660f10c6d74b07f
Amiy Shukla <amiyshukla1@gmail.com>
2025-11-30 10:29:42 +0530

    Add Leap Year checking Program
    This program checks whether a given year is a leap year using the correct rules  Divisible by 400  leap year
    Divisible by 4 but not 100  leap year Otherwise not a leap year

---
+++ leap Year.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+//WAP To check whether the entered year is a leap or not//
+
+int main()
+{
+    int year;
+    printf("Enter your Year: ");
+    scanf("%d",&year);
+
+    if(year%400 == 0)
+    {
+        printf("%d is a Leap Year.",year);
+    }
+    else if(year%4 == 0 && year%100 != 0)
+    {
+        printf("%d is a Leap Year.",year);
+    }
+    else
+    {
+        printf("%d is not a Leap Year.",year);
+    }
+
+    return 0;
+}
